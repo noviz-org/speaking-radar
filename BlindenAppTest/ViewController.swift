95d8e56d5e1b56f465a293513a7f5e2946f5be88
Lukas Reitemeier <Reitemeier@uwes-imac.local>
2017-12-02 17:24:16 +0100

    First commit

--- BlindenAppTest/ViewController.swift
+++ BlindenAppTest/ViewController.swift
@@ -1,25 +1,94 @@
 //
 //  ViewController.swift
-//  BlindenAppTest
+//  userLocation
 //
-//  Created by Lukas Reitemeier on 20.10.17.
-//  Copyright © 2017 Lukas Reitemeier. All rights reserved.
+//  Created by Sebastian Hette on 19.09.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate
+{
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLocation: UILabel!
+    @IBOutlet weak var compassLabel: UILabel!
+    
+    @IBOutlet weak var winkelEingabe: UITextField!
+    
+    @IBOutlet weak var Button1: UIButton!
+    
+    var i = 1
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        print(location.course)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        latitudeLabel.text = String(location.coordinate.latitude)
+        longitudeLabel.text = String(location.coordinate.longitude)
+        courseLocation.text = String(location.course)
+        
+        self.map.showsUserLocation = true
+
+        i = i+1
+        print("LocationManager func did run \(i) times")
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading)
+    {
+        let x = newHeading.x
+        let y = newHeading.y
+        let z = newHeading.z
+        
+        // Sets the text of the label to the three compass coordinates
+        compassLabel.text = String("(\(x),\(y),\(z))");
+        print("Compass: (\(x),\(y),\(z))")
+    }
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        manager.startUpdatingHeading()
+        
+        
+        funExt()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func funExt () {
+        let poii: Poi = Poi()
+        var textString: String = "sdg"
+        textString = poii.name
+        //textString = "abcd"
+        
+        print(textString)
+    }
+    
 }
+
 
---
+++ BlindenAppTest/poi.swift
@@ -0,0 +1,27 @@
+//
+//  poi.swift
+//  BlindenAppTest
+//
+//  Created by Lukas Reitemeier on 22.10.17.
+//  Copyright © 2017 Lukas Reitemeier. All rights reserved.
+//
+
+import Foundation
+
+class Poi
+{
+    var name: String = "poi"
+    var long: Int
+    var lat: Int
+    static var test: String = "Hello from the otter slide."
+    
+    init()
+    {
+        long = 0;
+        lat = 0;
+    }
+    
+    
+    
+}
+
