117f6bba30aacde8d33778c0a4fda4a44230f142
Lukas Reitemeier <lukas.reitemeier@hotmail.com>
2018-02-24 11:12:53 +0100

    test

--- BlindenAppTest/ViewController.swift
+++ BlindenAppTest/ViewController.swift
@@ -54,7 +54,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate
         let x = newHeading.x
         let y = newHeading.y
         let z = newHeading.z
-        
+        // change
         // Sets the text of the label to the three compass coordinates
         compassLabel.text = String("(\(x),\(y),\(z))");
         print("Compass: (\(x),\(y),\(z))")
