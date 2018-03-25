c887d9dbc28a752ed1b5d25880f0933593667ccc
Lukas Reitemeier <lukas.reitemeier@hotmail.com>
2018-03-25 12:16:44 +0200

    copied code to recognize all swipe directions

--- BlindenApp/ViewController.swift
+++ BlindenApp/ViewController.swift
@@ -131,9 +131,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate
         circle.fillColor = UIColor.blue.cgColor
         self.view.layer.addSublayer(circle)
         
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
         
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
     }
-    
+
+func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+    if gesture.direction == UISwipeGestureRecognizerDirection.right {
+        print("Swipe Right")
+    }
+    else if gesture.direction == UISwipeGestureRecognizerDirection.left {
+        print("Swipe Left")
+    }
+    else if gesture.direction == UISwipeGestureRecognizerDirection.up {
+        print("Swipe Up")
+    }
+    else if gesture.direction == UISwipeGestureRecognizerDirection.down {
+        print("Swipe Down")
+    }
+}
+
     func circlePathWithCenter(center: CGPoint, radius: CGFloat) -> UIBezierPath {
         let circlePath = UIBezierPath()
         circlePath.addArc(withCenter: center, radius: radius, startAngle: -CGFloat(M_PI), endAngle: -CGFloat(M_PI/2), clockwise: true)
